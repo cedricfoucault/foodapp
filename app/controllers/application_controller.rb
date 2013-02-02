f129b31054a8d84520f49aea19306a907e76535c
Hashmal <jeremypinat@gmail.com>
2013-02-02 14:59:30 +0100

    Move authentication into a before_filter
    A before_filter executes code before a controller, it is the perfect
    place to put authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def auth
+    unless request.headers['Authorization'] == '2108'
+      respond_to do |format|
+        format.xml { head :unauthorized }
+      end
+    end
+  end
 end
 
