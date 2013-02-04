080af0f212638581ba0cad75688ada8ab5b6b7fb
Hashmal <jeremypinat@gmail.com>
2013-02-04 01:09:38 +0100

    Add info pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,81 @@
 //= require jquery
 //= require_tree .
 
+window.TEXT_FIELDS = [
+    "common_name",
+    "manufacturer_name",
+    "scientific_name",
+    "category",
+    "short_description",
+    "long_description",
+    "refuse_description",
+    "source"
+]
+
+window.FLOAT_FIELDS = [
+    "refuse_percentage",
+    "density",
+    "energy",
+    "water",
+    "protein",
+    "carbohydrate",
+    "fat",
+    "fibers",
+    "sugar",
+    "alcohol",
+    "caffeine",
+    "saturated_fat",
+    "monounsaturated_fat",
+    "polyunsaturated_fat",
+    "trans_fat",
+    "cholesterol",
+    "omega_3",
+    "epa",
+    "dpa",
+    "dha",
+    "ala",
+    "omega_6",
+    "calcium",
+    "iron",
+    "magnesium",
+    "phosphorus",
+    "potassium",
+    "sodium",
+    "zinc",
+    "copper",
+    "manganese",
+    "selenium",
+    "fluoride",
+    "vit_a",
+    "vit_b1",
+    "vit_b2",
+    "vit_b3",
+    "vit_b5",
+    "vit_b6",
+    "vit_b7",
+    "vit_b9",
+    "vit_b12",
+    "vit_c",
+    "vit_d",
+    "vit_e",
+    "vit_k",
+    "alpha_carotene",
+    "beta_carotene"
+]
+
+function updateChart (summary) {
+    window.overview.series[0].data[0].update(summary.protein()*4);
+    window.overview.series[0].data[1].update(summary.carbohydrate()*4);
+    window.overview.series[0].data[2].update(summary.fat()*9);
+}
+
+function updateDetailChart (food) {
+    window.detailChart.series[0].data[0].update(food.protein*4);
+    window.detailChart.series[0].data[1].update(food.carbohydrate*4);
+    window.detailChart.series[0].data[2].update(food.fat*9);
+}
+
+// Ajouter un event se déclenchant à chaque touche de clavier.
 angular.module('FoodApp', []).
 directive('onKeyup', function() {
     return function(scope, elm, attrs) {
@@ -28,66 +103,109 @@ function FoodCtrl($scope) {
     $scope.foods = [];
     $scope.currentFood = {};
 
-    // bilan des protéines
-    $scope.proteins = function() {
-        var r = 0;
-        for (var i = $scope.foods.length - 1; i >= 0; i--){
-            r = r + ($scope.foods[i].proteins * $scope.foods[i].quantity / 100.0);
-        };
-        return r;
+    ///////////////////////
+    //  CALCUL DU BILAN  //
+    ///////////////////////
+
+    function createFunc (j) {
+        return function () {
+            var r = 0;
+            for (var i = $scope.foods.length - 1; i >= 0; i--){
+                r = r + ($scope.foods[i][FLOAT_FIELDS[j]] * $scope.foods[i].quantity / 100.0);
+            };
+            return r;
+        }
+    }
+
+    for (var j = FLOAT_FIELDS.length - 1; j >= 0; j--){
+        $scope[FLOAT_FIELDS[j]] = createFunc(j);
     };
 
-    // bilan des glucides
-    $scope.carbs = function() {
-        var r = 0;
-        for (var i = $scope.foods.length - 1; i >= 0; i--){
-            r = r + ($scope.foods[i].carbs * $scope.foods[i].quantity / 100.0);
-        };
-        return r;
-    };
+    ///////////////////////////////////
+    //  MANIPULATION DE L'INTERFACE  //
+    ///////////////////////////////////
 
-    // bilan des lipides
-    $scope.fat = function() {
-        var r = 0;
-        for (var i = $scope.foods.length - 1; i >= 0; i--){
-            r = r + ($scope.foods[i].fat * $scope.foods[i].quantity / 100.0);
-        };
-        return r;
-    };
+    $scope.showSearchBox = function () {
+        $('#searchBox').removeClass("hidden");
+        $('#confirmBox').addClass("hidden");
+    }
 
-    $scope.showDetail = function() {
+    // Montrer la page détaillée d'un aliment.
+    $scope.showDetail = function(food) {
+        $scope.currentFood = food;
+        updateDetailChart(food);
         $(".detail").removeClass("hidden");
     };
 
+    // Masquer la page détaillée (retour à la page principale).
     $scope.hideDetail = function() {
         $(".detail").addClass("hidden");
+        $(".summaryDetail").addClass("hidden");
+    };
+
+    // Montrer le bilan détaillé.
+    $scope.showSummaryDetail = function() {
+        // updateDetailChart(food);
+        $("#summaryPie").html($("#pie").html());
+        $(".summaryDetail").removeClass("hidden");
     };
 
     $scope.searchFood = function() {
-        if ($scope.foodText.length > 2) {
-            $.ajax("/foods.xml?name=" + $scope.foodText, {
-                success: function (r) {
-                    // console.log(r);
-                    $scope.results = [];
-
-                    results = $(r).find("food");
-
-                    results.each(function(i) {
-                        $scope.results.push({
-                            text:     $(results[i]).find("long_description").text(),
-                            proteins: parseFloat($(results[i]).find("protein").text()),
-                            carbs:    parseFloat($(results[i]).find("carbohydrate").text()),
-                            fat:      parseFloat($(results[i]).find("fat").text())
-                        });
-                    });
-
-                    $('.completion').removeClass("hidden");
-                    $scope.$apply();
-                }
-            });
-        } else {
+        if ($scope.foodText.length <= 2) {
             $('.completion').addClass("hidden");
+            return;
+        }
+
+        $.ajax("/foods.xml?name=" + $scope.foodText, {
+            success: function (r) {
+                $scope.results = [];
+                results = $(r).find("food");
+
+                results.each(function(i) {
+                    newFood = {};
+                    for (var j = TEXT_FIELDS.length - 1; j >= 0; j--) {
+                        newFood[TEXT_FIELDS[j]] = $(results[i]).find(TEXT_FIELDS[j]).text();
+                    };
+                    for (var j = FLOAT_FIELDS.length - 1; j >= 0; j--) {
+                        newFood[FLOAT_FIELDS[j]] = parseFloat($(results[i]).find(FLOAT_FIELDS[j]).text());
+                    };
+                    // console.log(newFood);
+                    $scope.results.push(newFood);
+
+                    // $scope.results.push({
+                    //     text:     $(results[i]).find("long_description").text(),
+                    //     proteins: parseFloat($(results[i]).find("protein").text()),
+                    //     carbs:    parseFloat($(results[i]).find("carbohydrate").text()),
+                    //     fat:      parseFloat($(results[i]).find("fat").text())
+                    // });
+                });
+
+                $('.completion').removeClass("hidden");
+                $scope.$apply();
+                $('#quantity')[0].focus();
+            }
+        });
+    };
+
+    // cette fonction est executée quand la fonction de recherche est
+    // validée.
+    $scope.addFood = function() {
+        $scope.currentFood.quantity = parseFloat($scope.quantity);
+        $scope.foods.push($scope.currentFood);
+        $scope.showSearchBox();
+        $scope.foodText = '';
+        $scope.quantity = '';
+        updateChart($scope);
+    };
+
+    // retirer un aliment de la liste.
+    $scope.removeFood = function(food) {
+        for (var i = $scope.foods.length - 1; i >= 0; i--){
+            if(food == $scope.foods[i]) {
+                $scope.foods.splice(i, 1);
+            }
         };
+        updateChart($scope);
     };
 
     $scope.selectFood = function (e) {
@@ -97,53 +215,6 @@ function FoodCtrl($scope) {
         $('#confirmBox').removeClass("hidden");
         $scope.foodText = '';
     }
-
-    // cette fonction est executée quand la fonction de recherche est
-    // validée.
-    $scope.addFood = function() {
-        // $.ajax("/foods/" + $scope.foodText + ".xml", {
-        //     success: function (r) {
-        //         r = $(r);
-        //         // TODO: traiter la réponse (<food>...</food>)
-        // 
-        //         // ajouter les donnéees extraites à la liste d'aliments:
-        //         $scope.foods.push({
-        //             text:     r.find("common_name").text(),
-        //             proteins: 0, // 0 étant une valeur bateau pour le moment
-        //             carbs:    0,
-        //             fat:      0
-        //         });
-        //         $scope.$apply();
-        //         // $('.dynamicsparkline').sparkline("html", {
-        //         //   type: "pie"
-        //         // });
-        //     }
-        // });
-
-        $scope.currentFood.quantity = parseFloat($scope.quantity);
-        $scope.foods.push($scope.currentFood);
-        $('#searchBox').removeClass("hidden");
-        $('#confirmBox').addClass("hidden");
-        $scope.foodText = '';
-        $scope.quantity = '';
-        window.overview.series[0].data[0].update($scope.proteins());
-        window.overview.series[0].data[1].update($scope.carbs());
-        window.overview.series[0].data[2].update($scope.fat());
-    };
-
-    $scope.cancel = function () {
-        $('#searchBox').removeClass("hidden");
-        $('#confirmBox').addClass("hidden");
-    }
-
-    // retirer un aliment de la liste
-    $scope.removeFood = function(food) {
-        for (var i = $scope.foods.length - 1; i >= 0; i--){
-            if(food == $scope.foods[i]) {
-                $scope.foods.splice(i, 1);
-            }
-        };
-    };
 };
 
 // la page est divisee en 3 sections (resume des nutriments, liste
